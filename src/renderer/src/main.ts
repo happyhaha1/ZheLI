25b46d5ffea7be61e100383a224a4fde358481ab
happyhaha1 <474248257@qq.com>
2023-03-11 22:23:26 +0800

    加入devtools

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,4 +1,7 @@
+import devtools from '@vue/devtools'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+if (import.meta.env.DEV) devtools.connect('10.10.10.3', 8098)
